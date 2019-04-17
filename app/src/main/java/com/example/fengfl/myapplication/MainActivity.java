f5f35e02b42a7122e6a76b38eefa18cf1c9adf79
fengfeilong <fengfeilongv5@163.com>
2019-04-17 09:35:42 +0800

    b1 modify  2!

--- app/src/main/java/com/example/fengfl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fengfl/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //b1 modify
+        //m2
     }
 }
